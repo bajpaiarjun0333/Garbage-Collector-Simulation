6966c357a7977f47a8c4d00bc6289d26aa41aa67
Arjun Bajpai <bajpaiarjun0333@gmail.com>
2023-08-14 11:51:54 +0530

    Started

--- gc.cpp
+++ gc.cpp
@@ -7,6 +7,13 @@ using namespace std;
 int heap_capacity=64000;//capacity in bytes: This is the total amount of memory that we can use.
 //this heap capacity should be divisible by pointer size in the machine so that we can get the total number of words
 int heap_capacity_words=(heap_capacity/sizeof(void*));
+//wecan store roughly 16000 words in the memory of our heap
+//we have to design the allocation, deacclocation of the memory as well
+
+int heap_filed=0;//number of words already there 
+cout<<"HI"<<endl;
+
+
 
 
 int main(){
